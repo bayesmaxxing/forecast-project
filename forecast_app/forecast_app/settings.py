bcad5f838bcc812a2887eef984c605f9906f3054
bayesmaxxing <bayesmaxxing@gmail.com>
2024-04-07 22:40:57 +0200

    cors change

--- forecast_app/forecast_app/settings.py
+++ forecast_app/forecast_app/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
-    'corsheaders',
+    'django-cors-headers',
     'forecaster',
     'rest_framework',
     'rest_framework.authtoken',
