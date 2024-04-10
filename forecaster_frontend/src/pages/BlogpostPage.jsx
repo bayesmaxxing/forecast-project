168fb0f0e309cbe0a945a66518cf77c3f58bc5a7
bayesmaxxing <bayesmaxxing@gmail.com>
2024-04-10 22:56:43 +0200

    fixed data display

--- forecaster_frontend/src/pages/BlogpostPage.jsx
+++ forecaster_frontend/src/pages/BlogpostPage.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Marked } from 'marked';
+import Markdown from 'marked-react';
 import './BlogpostPage.css';
 
 
 function BlogpostPage() {
-    const [blogpost, setBlogpost] = useState(null);
+    const [blogpost, setBlogpost] = useState({});
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     let { slug } = useParams();
@@ -16,42 +16,39 @@ function BlogpostPage() {
             'Authorization': `Token ${process.env.REACT_APP_API_TOKEN}`
           }
         })
-      .then( blogpostData => {
-        if (!blogpostData.ok) {
-          throw new Error('Error fetching data');
-        }
-        const blogpostJson =blogpostData.json()
-        return blogpostJson;
-      })
-      .then(blogpostJson => {
-        setBlogpost(blogpostJson);
-        setLoading(false);
-      })
-      .catch(error => {
-        setError(error);
-        setLoading(false);
-      });
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Error fetching data');
+          }
+          return response.json(); 
+        })
+        .then(data => { 
+          if (data.length > 0) {
+            setBlogpost(data[0]); 
+          } else {
+            throw new Error('No blog posts found');
+          }
+          setLoading(false);
+        })
+        .catch(error => {
+          setError(error);
+          setLoading(false);
+        });
     }, [slug]);
-  
+
+    
     if (loading) return <div>Loading...</div>;
     if (error) return <div>Error loading the forecast: {error.message}</div>;
-
     return (
       <div>
-          {blogpost ? ( // Check if the blogpost object is not null
               <article>
                   <header>
-                      <h1 className="blogpost-title">{blogpost.title}</h1>
-                      <div className="blogpost-meta">
-                      </div>
+                      <h1 className="blogpost-title">ASSASAS {blogpost.title}</h1>
                   </header>
                   <section className="blogpost-content">
-                      <p>{blogpost.post}</p> {/* Assuming 'content' is a field in your blogpost object */}
+                      <Markdown>{blogpost.post}</Markdown>
                   </section>
               </article>
-          ) : (
-              <div>Blog post not found.</div>
-          )}
       </div>
   );
   
