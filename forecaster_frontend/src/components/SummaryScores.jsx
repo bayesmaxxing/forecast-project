7c1bfa291167cbf31d5db52a0cced68f4f645af1
bayesmaxxing <bayesmaxxing@gmail.com>
2024-09-13 23:20:32 +0200

    refactor to use Go endpoints

--- forecaster_frontend/src/components/SummaryScores.jsx
+++ forecaster_frontend/src/components/SummaryScores.jsx
@@ -7,70 +7,75 @@ import 'chartjs-plugin-annotation';
 
 
 function SummaryScores() {
+    const [scores, setScores] = useState([]);
     const [resolutions, setResolutions] = useState([]);
     const [selectedMetric, setSelectedMetric] = useState('brier_score');
-    const [averageScore, setAverageScore] = useState(0);
+  
+    const navigate = useNavigate();
 
     useEffect(() => {
       const CACHE_DURATION = 5 * 60 * 1000; // Cache duration in milliseconds, e.g., 5 minutes
       const now = new Date().getTime(); // Current time
 
-      const resolutionsCache = localStorage.getItem('resolutions_scores');
+      const forecastsCache = localStorage.getItem('forecasts_resolved');
+      const scoresCache = localStorage.getItem('scores');
+      
 
-      const resolutionsDataValid = resolutionsCache && now - JSON.parse(resolutionsCache).timestamp < CACHE_DURATION;
+      const forecastDataValid = forecastsCache && now - JSON.parse(forecastsCache).timestamp < CACHE_DURATION;
       // Try to load data from cache
-      if (resolutionsDataValid) {
-        setResolutions(JSON.parse(resolutionsCache).data);
+      if (forecastDataValid) {
+        setResolutions(JSON.parse(forecastsCache).data);
+        setScores(JSON.parse(scoresCache).data);
       } else {
         // Fetch the list of resolutions from the API if cache is empty
+        Promise.all([
         fetch('http://localhost:8080/scores', {
             headers : {
-              'Authorization': `Token ${process.env.REACT_APP_API_TOKEN}`
+              'Accept': 'application/json'
+            }
+          }),
+          fetch('http://localhost:8080/forecasts?type=resolved', {
+            headers : {
+              'Accept': 'application/json'
             }
           })
-          .then(response => response.json())
-          .then(data => {
-            // Update state with fetched data
-            setResolutions(data);
-            // Update cache with new data
-            localStorage.setItem('resolutions_scores', JSON.stringify({data: data, timestamp: now}));
+        ])
+          .then(async ([scoreData, forecastData]) => {
+            const scoreDataJson = await scoreData.json();
+            const forecastDataJson = await forecastData.json();
+            return [scoreDataJson, forecastDataJson];
+          })
+          .then(([scoreDataJson, forecastDataJson]) => {
+            setScores(scoreDataJson);
+            setResolutions(forecastDataJson);
+
+            localStorage.setItem('scores', JSON.stringify({data:scoreDataJson, timestamp: now}));
+            localStorage.setItem('forecasts_resolved', JSON.stringify({data:forecastDataJson, timestamp: now}));
           })
           .catch(error => console.error('Error fetching data: ', error));
       }
     }, []);
 
-    useEffect(() => {
-      // Depending on the selected metric, calculate the appropriate average score
-      let currentScores;
-      switch(selectedMetric) {
-          case 'brier_score':
-              currentScores = resolutions.map(resolution => resolution.brier_score);
-              break;
-          case 'log2_score':
-              currentScores = resolutions.map(resolution => resolution.log2_score);
-              break;
-          case 'logn_score':
-              currentScores = resolutions.map(resolution => resolution.logn_score);
-              break;
-          default:
-              currentScores = [];
-      }
-      const average = calculateAverage(currentScores);
-      setAverageScore(average);
-  }, [selectedMetric, resolutions]);
-    
-    const navigate = useNavigate();
+    const getScore = () => {
+      if (!scores) return 0;
 
-    const calculateAverage = (scores) => {
-      if (scores.length === 0) {
-        return 0;
+      switch(selectedMetric) {
+        case 'brier_score':
+          return scores.AggBrierScore ?? 0;
+        case 'log2_score':
+          return scores.AggLog2Score ?? 0;
+        case 'logn_score':
+          return scores.AggLogNScore ?? 0;
+        default:
+          return scores.AggBrierScore ?? 0;
       }
-      const sum = scores.reduce((a, b) => a + b, 0);
-      return sum / scores.length;
     };
- 
+
+    const avgScore = getScore();
+
+    
     const metricScores = resolutions.map(resolution => resolution[selectedMetric]);
-    const labels = resolutions.map(resolution => `Forecast ${resolution.forecast}`);
+    const labels = resolutions.map(resolution => `Forecast ${resolution.id}`);
 
   
     const data = {
@@ -90,7 +95,7 @@ function SummaryScores() {
       onClick: (event, elements, chart) => {
         if (elements.length > 0) {
           const elementIndex = elements[0].index;
-          const forecastId = resolutions[elementIndex].forecast; // Ensure this matches your data structure
+          const forecastId = resolutions[elementIndex].id; // Ensure this matches your data structure
           navigate(`/forecast/${forecastId}`); // Adjust the path as needed
         }
       },
@@ -116,7 +121,7 @@ function SummaryScores() {
           <option value="log2_score">Binary Log Score</option>
           <option value="logn_score">Natural Log Score</option>
         </select>
-        <p>Average Score: {averageScore.toFixed(3)}</p>
+        <p>Average Score: {avgScore.toFixed(3)}</p>
         <Line data={data} options={options} />
       </div>
     );
