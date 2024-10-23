4e9b1061427f736c368bf07e9e2a6fe6a866ccca
bayesmaxxing <bayesmaxxing@gmail.com>
2024-10-23 22:03:25 +0200

    wrap app in theme + css background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ThemeProvider, CssBaseline, Box } from '@mui/material';
+import theme from './theme';
 import { Route, Routes, BrowserRouter} from 'react-router-dom';
 import ForecastPage from './pages/ForecastPage';
 import FilteredForecastPage from './pages/FilteredForecastPage';
@@ -14,22 +16,30 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes>  
-          <Route path="/" element={<HomePage />}/>
-          <Route path="/questions" element={<ForecastPage />}/>
-          <Route path="/forecast/:id" element={<SpecificForecast />}/>
-          <Route path="/questions/category/:category" element={<FilteredForecastPage />}/>
-          <Route path="/questions/resolved" element={<ResolvedForecastPage />}/>
-          <Route path="/faq" element={<FaqPage />}/>
-          <Route path='/blog' element={<BlogPage />}/>
-          <Route path='/blog/:slug' element={<BlogpostPage />}/>
-          <Route path='/admin' element={<AdminPage />}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{
+        bgcolor: 'background.default',
+        minHeight: '100vh'
+      }}>
+        <BrowserRouter>
+          <div className="App">
+            <Header />
+              <Routes>  
+                <Route path="/" element={<HomePage />}/>
+                <Route path="/questions" element={<ForecastPage />}/>
+                <Route path="/forecast/:id" element={<SpecificForecast />}/>
+                <Route path="/questions/category/:category" element={<FilteredForecastPage />}/>
+                <Route path="/questions/resolved" element={<ResolvedForecastPage />}/>
+                <Route path="/faq" element={<FaqPage />}/>
+                <Route path='/blog' element={<BlogPage />}/>
+                <Route path='/blog/:slug' element={<BlogpostPage />}/>
+                <Route path='/admin' element={<AdminPage />}/>
+              </Routes>
+          </div>
+        </BrowserRouter>
+      </Box>
+    </ThemeProvider>
   );
 }
 
