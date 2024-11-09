ee06680f0ab79801249e67b666a0fa7f84fb85c1
bayesmaxxing <bayesmaxxing@gmail.com>
2024-11-09 13:24:54 +0100

    add comma to make compile

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -57,7 +57,7 @@ const Sidebar = () => {
     { text: 'X-risk', path: '/questions/category/x-risk', icon: <Dangerous /> },
     { text: 'Sports', path: '/questions/category/sports', icon: <SportsBasketball /> },
     { text: 'Personal', path: '/questions/category/personal', icon: <Category /> },
-    { text: 'One-shot', path: '/questions/category/one-shot', icon: <TimesOneMobileData />}
+    { text: 'One-shot', path: '/questions/category/one-shot', icon: <TimesOneMobileData />},
     { text: 'Other', path: '/questions/category/other', icon: <Category /> },
     { text: 'Resolved', path: '/questions/resolved', icon: <Timeline /> } 
   ];
