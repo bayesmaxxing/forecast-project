be6757b215ab439695ee725d3c8340f994e08737
bayesmaxxing <bayesmaxxing@gmail.com>
2025-02-18 21:51:52 +0100

    create score repo

--- backend/internal/repository/scores_repository.go
+++ backend/internal/repository/scores_repository.go
@@ -5,6 +5,7 @@ import (
 	"backend/internal/models"
 	"context"
 	"database/sql"
+	"time"
 
 	_ "github.com/jackc/pgx/v5"
 )
@@ -55,7 +56,36 @@ func (r *ScoreRepository) GetScoreByForecastID(ctx context.Context, forecast_id 
 	return scores, rows.Err()
 }
 
+func (r *ScoreRepository) GetScoreByForecastAndUser(ctx context.Context, forecast_id int64, user_id int64) (*models.Scores, error) {
+	query := `SELECT 
+					id 
+					, brier_score
+					, log2_score
+					, logn_score
+					, user_id
+					, forecast_id
+					, created
+					FROM scores 
+					WHERE forecast_id = $1 AND user_id = $2`
+
+	var score models.Scores
+	err := r.db.QueryRowContext(ctx, query, forecast_id, user_id).Scan(
+		&score.ID,
+		&score.BrierScore,
+		&score.Log2Score,
+		&score.LogNScore,
+		&score.UserID,
+		&score.ForecastID,
+		&score.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return &score, nil
+}
+
 func (r *ScoreRepository) CreateScore(ctx context.Context, score *models.Scores) error {
+	score.CreatedAt = time.Now()
+
 	query := `INSERT INTO scores (brier_score, log2_score, logn_score, user_id, forecast_id, created)
               VALUES ($1, $2, $3, $4, $5, $6)
               RETURNING id`
@@ -69,14 +99,14 @@ func (r *ScoreRepository) CreateScore(ctx context.Context, score *models.Scores)
 		score.CreatedAt).Scan(&score.ID)
 }
 
-func (r *ScoreRepository) GetScoresByUserID(ctx context.Context, userID int64) ([]models.Scores, error) {
+func (r *ScoreRepository) GetScoresByUserID(ctx context.Context, user_id int64) ([]models.Scores, error) {
 	query := `SELECT 
 				id, brier_score, log2_score, logn_score, user_id, forecast_id, created
 			  FROM scores 
 			  WHERE user_id = $1
 			  ORDER BY created DESC`
 
-	rows, err := r.db.QueryContext(ctx, query, userID)
+	rows, err := r.db.QueryContext(ctx, query, user_id)
 	if err != nil {
 		return nil, err
 	}
@@ -156,10 +186,10 @@ func (r *ScoreRepository) UpdateScore(ctx context.Context, score *models.Scores)
 	return nil
 }
 
-func (r *ScoreRepository) DeleteScore(ctx context.Context, scoreID int64) error {
+func (r *ScoreRepository) DeleteScore(ctx context.Context, score_id int64) error {
 	query := `DELETE FROM scores WHERE id = $1`
 
-	result, err := r.db.ExecContext(ctx, query, scoreID)
+	result, err := r.db.ExecContext(ctx, query, score_id)
 	if err != nil {
 		return err
 	}
@@ -175,9 +205,9 @@ func (r *ScoreRepository) DeleteScore(ctx context.Context, scoreID int64) error 
 }
 
 // Aggregate score operations
+// all users
 func (r *ScoreRepository) GetOverallScores(ctx context.Context) (*models.OverallScores, error) {
-	query := `WITH user_scores AS (
-				SELECT 
+	query := `SELECT 
 				AVG(brier_score) as avg_brier,
 				AVG(log2_score) as avg_log2,
 				AVG(logn_score) as avg_logn,
@@ -201,8 +231,7 @@ func (r *ScoreRepository) GetOverallScores(ctx context.Context) (*models.Overall
 }
 
 func (r *ScoreRepository) GetCategoryScores(ctx context.Context, category string) (*models.CategoryScores, error) {
-	query := `WITH category_scores AS (
-				SELECT 
+	query := `SELECT 
 				AVG(brier_score) as avg_brier,
 				AVG(log2_score) as avg_log2,
 				AVG(logn_score) as avg_logn,
@@ -211,7 +240,7 @@ func (r *ScoreRepository) GetCategoryScores(ctx context.Context, category string
 			  FROM scores s
 			  JOIN forecast_v2 f
 			  ON s.forecast_id = f.id
-			  WHERE f.category = $1`
+			  WHERE f.category LIKE ($1)`
 
 	categoryPattern := "%" + category + "%"
 
@@ -230,31 +259,79 @@ func (r *ScoreRepository) GetCategoryScores(ctx context.Context, category string
 	return &categoryScores, nil
 }
 
-func (r *ScoreRepository) GetUserScores(ctx context.Context, userID int64) (*models.UserScores, error) {
+func (r *ScoreRepository) GetCategoryScoresByUsers(ctx context.Context, category string) ([]models.UserCategoryScores, error) {
+	query := `SELECT 
+				AVG(s.brier_score) as avg_brier,
+				AVG(s.log2_score) as avg_log2,
+				AVG(s.logn_score) as avg_logn,
+				s.user_id,
+				COUNT(DISTINCT s.forecast_id) as total_forecasts
+			  FROM scores s
+			  JOIN forecast_v2 f
+			  ON s.forecast_id = f.id
+			  WHERE f.category LIKE ($1)
+			  GROUP BY s.user_id`
+
+	categoryPattern := "%" + category + "%"
+
+	rows, err := r.db.QueryContext(ctx, query, categoryPattern)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var userCategoryScores []models.UserCategoryScores
+	for rows.Next() {
+		var u models.UserCategoryScores
+		if err := rows.Scan(
+			&u.BrierScore,
+			&u.Log2Score,
+			&u.LogNScore,
+			&u.UserID,
+			&u.Category,
+			&u.TotalForecasts,
+		); err != nil {
+			return nil, err
+		}
+		userCategoryScores = append(userCategoryScores, u)
+	}
+	return userCategoryScores, rows.Err()
+}
+
+func (r *ScoreRepository) GetOverallScoresByUsers(ctx context.Context) ([]models.UserScores, error) {
 	query := `SELECT 
 				AVG(brier_score) as avg_brier,
 				AVG(log2_score) as avg_log2,
 				AVG(logn_score) as avg_logn,
+				user_id,
 				COUNT(DISTINCT forecast_id) as total_forecasts
 			  FROM scores
-			  WHERE user_id = $1`
+			  GROUP BY user_id`
 
-	var userScores models.UserScores
-	err := r.db.QueryRowContext(ctx, query, userID).Scan(
-		&userScores.BrierScore,
-		&userScores.Log2Score,
-		&userScores.LogNScore,
-		&userScores.TotalForecasts,
-	)
+	rows, err := r.db.QueryContext(ctx, query)
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
-	userScores.UserID = userID
-
-	return &userScores, nil
+	var userScores []models.UserScores
+	for rows.Next() {
+		var u models.UserScores
+		if err := rows.Scan(
+			&u.BrierScore,
+			&u.Log2Score,
+			&u.LogNScore,
+			&u.UserID,
+			&u.TotalForecasts,
+		); err != nil {
+			return nil, err
+		}
+		userScores = append(userScores, u)
+	}
+	return userScores, rows.Err()
 }
 
+// user-specific
 func (r *ScoreRepository) GetUserCategoryScores(ctx context.Context, userID int64, category string) (*models.UserCategoryScores, error) {
 	query := `SELECT 
 				AVG(brier_score) as avg_brier,
@@ -264,10 +341,12 @@ func (r *ScoreRepository) GetUserCategoryScores(ctx context.Context, userID int6
 			  FROM scores s
 			  JOIN forecast_v2 f
 			  ON s.forecast_id = f.id
-			  WHERE s.user_id = $1 AND f.category = $2`
+			  WHERE s.user_id = $1 AND f.category LIKE ($2)`
+
+	categoryPattern := "%" + category + "%"
 
 	var userCategoryScores models.UserCategoryScores
-	err := r.db.QueryRowContext(ctx, query, userID, category).Scan(
+	err := r.db.QueryRowContext(ctx, query, userID, categoryPattern).Scan(
 		&userCategoryScores.BrierScore,
 		&userCategoryScores.Log2Score,
 		&userCategoryScores.LogNScore,
@@ -278,4 +357,30 @@ func (r *ScoreRepository) GetUserCategoryScores(ctx context.Context, userID int6
 	}
 
 	return &userCategoryScores, nil
+}
+
+func (r *ScoreRepository) GetUserOverallScores(ctx context.Context, user_id int64) (*models.UserScores, error) {
+	query := `SELECT 
+				AVG(brier_score) as avg_brier,
+				AVG(log2_score) as avg_log2,
+				AVG(logn_score) as avg_logn,
+				COUNT(DISTINCT forecast_id) as total_forecasts
+			  FROM scores s
+			  JOIN forecast_v2 f
+			  ON s.forecast_id = f.id
+			  WHERE s.user_id = $1`
+
+	var userScores models.UserScores
+	err := r.db.QueryRowContext(ctx, query, user_id).Scan(
+		&userScores.BrierScore,
+		&userScores.Log2Score,
+		&userScores.LogNScore,
+		&userScores.UserID,
+		&userScores.TotalForecasts,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &userScores, nil
 }
