d5456d88211af671f6870b0336de2189931c923b
bayesmaxxing <bayesmaxxing@gmail.com>
2025-03-04 23:04:53 +0100

    change routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,12 @@ import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import theme from './theme';
 import { Route, Routes, BrowserRouter} from 'react-router-dom';
 import ForecastPage from './pages/ForecastPage';
-import FilteredForecastPage from './pages/FilteredForecastPage';
-import ResolvedForecastPage from './pages/ResolvedForecastPage';
 import SpecificForecast from './pages/SpecificForecast';
 import HomePage from './pages/HomePage';
 import FaqPage from './pages/faqPage';
 import BlogPage from './pages/BlogPage';
 import BlogpostPage from './pages/BlogpostPage';
-import Header from './components/header';
+import Header from './components/Header';
 import AdminPage from './pages/AdminPage';
 
 function App() {
@@ -28,8 +26,8 @@ function App() {
                 <Route path="/" element={<HomePage />}/>
                 <Route path="/questions" element={<ForecastPage />}/>
                 <Route path="/forecast/:id" element={<SpecificForecast />}/>
-                <Route path="/questions/category/:category" element={<FilteredForecastPage />}/>
-                <Route path="/questions/resolved" element={<ResolvedForecastPage />}/>
+                <Route path="/questions/category/:category" element={<ForecastPage />}/>
+                <Route path="/questions/resolved" element={<ForecastPage />}/>
                 <Route path="/faq" element={<FaqPage />}/>
                 <Route path='/blog' element={<BlogPage />}/>
                 <Route path='/blog/:slug' element={<BlogpostPage />}/>
