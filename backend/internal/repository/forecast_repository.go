bc7c5867e1675b8d964d2eb5804fa7a94aa5026c
bayesmaxxing <bayesmaxxing@gmail.com>
2025-08-01 23:15:17 +0200

    fetch non-resolved forecasts

--- backend/internal/repository/forecast_repository.go
+++ backend/internal/repository/forecast_repository.go
@@ -251,11 +251,11 @@ func (r *PostgresForecastRepository) GetStaleAndNewForecasts(ctx context.Context
 							, f.resolution_criteria
 							, f.resolution
 							, f.resolved
-							, f.comment 
+							, f.comment
 							FROM forecasts f
 							LEFT JOIN latest_forecast_points lfp
 							ON f.id = lfp.forecast_id
-							WHERE f.resolved is not null
+							WHERE f.resolved is null
 							AND (lfp.forecast_id is null or lfp.latest_created < current_date - 14)`
 
 	return r.queryForecasts(ctx, query, userID)
