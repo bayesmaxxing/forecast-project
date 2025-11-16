122e359d1a5b53d6635738969389c71504851f8e
bayesmaxxing <bayesmaxxing@gmail.com>
2025-11-16 20:43:17 +0100

    refactor forecast point repo-layer to use a query-builder pattern

--- backend/internal/repository/forecast_points_repository.go
+++ backend/internal/repository/forecast_points_repository.go
@@ -4,6 +4,8 @@ import (
 	"backend/internal/database"
 	"backend/internal/models"
 	"context"
+	"fmt"
+	"strings"
 	"time"
 
 	_ "github.com/jackc/pgx/v5"
@@ -11,14 +13,8 @@ import (
 
 // ForecastPointRepository defines the interface for forecast point data operations
 type ForecastPointRepository interface {
-	GetAllForecastPoints(ctx context.Context) ([]*models.ForecastPoint, error)
-	GetForecastPointsByForecastID(ctx context.Context, id int64) ([]*models.ForecastPoint, error)
-	GetForecastPointsByForecastIDAndUser(ctx context.Context, id int64, userID int64) ([]*models.ForecastPoint, error)
-	GetOrderedForecastPointsByForecastID(ctx context.Context, id int64) ([]*models.ForecastPoint, error)
+	GetForecastPoints(ctx context.Context, filters models.PointFilters) ([]*models.ForecastPoint, error)
 	CreateForecastPoint(ctx context.Context, fp *models.ForecastPoint) error
-	GetLatestForecastPoints(ctx context.Context) ([]*models.ForecastPoint, error)
-	GetLatestForecastPointsByUser(ctx context.Context, userID int64) ([]*models.ForecastPoint, error)
-	GetForecastPointsByDate(ctx context.Context, userID int64, date *time.Time) ([]*models.ForecastPoint, error)
 }
 
 // PostgresForecastPointRepository implements the ForecastPointRepository interface
@@ -31,18 +27,86 @@ func NewForecastPointRepository(db *database.DB) ForecastPointRepository {
 	return &PostgresForecastPointRepository{db: db}
 }
 
-func (r *PostgresForecastPointRepository) GetAllForecastPoints(ctx context.Context) ([]*models.ForecastPoint, error) {
-	query := `SELECT 
-				p.id
-				, p.forecast_id
-				, p.point_forecast
-				, p.reason
-				, p.created
-				, p.user_id
-				, u.username
-				FROM points p
-				left join users u on p.user_id = u.id
-				ORDER BY p.created DESC`
+func buildForecastPointQuery(filters models.PointFilters) (string, error) {
+	// start with distinct on forecast if requested
+	// the fields to select are always the same
+	distinctClause := []string{}
+	if filters.DistinctOnForecast != nil && *filters.DistinctOnForecast {
+		distinctClause = append(distinctClause, "distinct on (forecast_id)")
+	} else {
+		distinctClause = append(distinctClause, "")
+	}
+
+	selectFields := []string{
+		"p.id",
+		"p.forecast_id",
+		"p.point_forecast",
+		"p.reason",
+		"p.created",
+		"p.user_id",
+		"u.username",
+	}
+
+	//so are from clauses
+	fromClauses := []string{
+		"points p",
+		"left join users u",
+		"on p.user_id = u.id",
+	}
+
+	// build where conditions
+	argsCounter := 1
+	whereConditions := []string{"1=1"}
+	if filters.UserID != nil {
+		whereConditions = append(whereConditions, "p.user_id = "+fmt.Sprintf("$%d", argsCounter))
+		argsCounter++
+	}
+	if filters.ForecastID != nil {
+		whereConditions = append(whereConditions, "p.forecast_id = "+fmt.Sprintf("$%d", argsCounter))
+		argsCounter++
+	}
+	if filters.Date != nil {
+		whereConditions = append(whereConditions, "p.created >= "+fmt.Sprintf("$%d", argsCounter))
+		argsCounter++
+		whereConditions = append(whereConditions, "p.created < "+fmt.Sprintf("$%d", argsCounter))
+		argsCounter++
+	}
+
+	// finally order by statements
+	orderBy := []string{}
+	if filters.OrderByForecastID != nil && *filters.OrderByForecastID {
+		orderBy = append(orderBy, "p.forecast_id")
+	}
+
+	if filters.CreatedDirection != nil {
+		orderBy = append(orderBy, "p.created "+*filters.CreatedDirection)
+	} else {
+		orderBy = append(orderBy, "p.created DESC")
+	}
+
+	// build query
+	query := fmt.Sprintf(
+		`select
+			%s
+			%s
+		from %s
+		where %s
+		order by %s`,
+		strings.Join(distinctClause, " "),
+		strings.Join(selectFields, ", "),
+		strings.Join(fromClauses, "\n\t\t"),
+		strings.Join(whereConditions, " and "),
+		strings.Join(orderBy, ", "),
+	)
+
+	return query, nil
+}
+
+func (r *PostgresForecastPointRepository) GetForecastPoints(ctx context.Context, filters models.PointFilters) ([]*models.ForecastPoint, error) {
+	query, err := buildForecastPointQuery(filters)
+	if err != nil {
+		return nil, err
+	}
 
 	rows, err := r.db.QueryContext(ctx, query)
 	if err != nil {
@@ -54,7 +118,6 @@ func (r *PostgresForecastPointRepository) GetAllForecastPoints(ctx context.Conte
 	var forecast_points []*models.ForecastPoint
 	for rows.Next() {
 		var fp models.ForecastPoint
-
 		if err := rows.Scan(&fp.ID,
 			&fp.ForecastID,
 			&fp.PointForecast,
@@ -66,81 +129,7 @@ func (r *PostgresForecastPointRepository) GetAllForecastPoints(ctx context.Conte
 		}
 		forecast_points = append(forecast_points, &fp)
 	}
-	return forecast_points, rows.Err()
-}
 
-func (r *PostgresForecastPointRepository) GetForecastPointsByForecastID(ctx context.Context, id int64) ([]*models.ForecastPoint, error) {
-	query := `SELECT 
-				p.id
-				, p.forecast_id
-				, p.point_forecast
-				, p.reason
-				, p.created
-				, p.user_id
-				, u.username
-				FROM points p
-				left join users u on p.user_id = u.id
-				WHERE p.forecast_id = $1`
-
-	rows, err := r.db.QueryContext(ctx, query, id)
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	var forecast_points []*models.ForecastPoint
-	for rows.Next() {
-		var fp models.ForecastPoint
-		if err := rows.Scan(&fp.ID,
-			&fp.ForecastID,
-			&fp.PointForecast,
-			&fp.Reason,
-			&fp.CreatedAt,
-			&fp.UserID,
-			&fp.UserName); err != nil {
-			return nil, err
-		}
-		forecast_points = append(forecast_points, &fp)
-	}
-	return forecast_points, rows.Err()
-}
-
-func (r *PostgresForecastPointRepository) GetForecastPointsByForecastIDAndUser(ctx context.Context, id int64, user_id int64) ([]*models.ForecastPoint, error) {
-	query := `SELECT 
-				p.id
-				, p.forecast_id
-				, p.point_forecast
-				, p.reason
-				, p.created
-				, p.user_id
-				, u.username
-				FROM points p
-				left join users u on p.user_id = u.id
-				WHERE p.forecast_id = $1
-				AND p.user_id = $2`
-
-	rows, err := r.db.QueryContext(ctx, query, id, user_id)
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	var forecast_points []*models.ForecastPoint
-	for rows.Next() {
-		var fp models.ForecastPoint
-		if err := rows.Scan(&fp.ID,
-			&fp.ForecastID,
-			&fp.PointForecast,
-			&fp.Reason,
-			&fp.CreatedAt,
-			&fp.UserID,
-			&fp.UserName); err != nil {
-			return nil, err
-		}
-		forecast_points = append(forecast_points, &fp)
-	}
 	return forecast_points, rows.Err()
 }
 
@@ -157,166 +146,4 @@ func (r *PostgresForecastPointRepository) CreateForecastPoint(ctx context.Contex
 
 	err := r.db.QueryRowContext(ctx, query, fp.ForecastID, fp.PointForecast, fp.CreatedAt, fp.Reason, fp.UserID).Scan(&fp.ID)
 	return err
-}
-
-func (r *PostgresForecastPointRepository) GetLatestForecastPoints(ctx context.Context) ([]*models.ForecastPoint, error) {
-	query := `SELECT distinct on (forecast_id)
-				p.id
-				, p.forecast_id
-				, p.point_forecast
-				, p.created
-				, p.reason
-				, p.user_id
-				, u.username
-				FROM points p
-				left join users u on p.user_id = u.id
-				ORDER BY p.forecast_id, p.created DESC;`
-
-	rows, err := r.db.QueryContext(ctx, query)
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	var forecast_points []*models.ForecastPoint
-	for rows.Next() {
-		var fp models.ForecastPoint
-		if err := rows.Scan(&fp.ID,
-			&fp.ForecastID,
-			&fp.PointForecast,
-			&fp.CreatedAt,
-			&fp.Reason,
-			&fp.UserID,
-			&fp.UserName); err != nil {
-			return nil, err
-		}
-		forecast_points = append(forecast_points, &fp)
-	}
-	return forecast_points, rows.Err()
-}
-
-func (r *PostgresForecastPointRepository) GetLatestForecastPointsByUser(ctx context.Context, user_id int64) ([]*models.ForecastPoint, error) {
-	query := `SELECT distinct on (forecast_id)
-				p.id
-				, p.forecast_id
-				, p.point_forecast
-				, p.created
-				, p.reason
-				, p.user_id
-				, u.username
-				FROM points p 
-				left join users u on p.user_id = u.id
-				WHERE p.user_id = $1
-				ORDER BY p.forecast_id, p.created DESC;`
-
-	rows, err := r.db.QueryContext(ctx, query, user_id)
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	var forecast_points []*models.ForecastPoint
-	for rows.Next() {
-		var fp models.ForecastPoint
-		if err := rows.Scan(&fp.ID,
-			&fp.ForecastID,
-			&fp.PointForecast,
-			&fp.CreatedAt,
-			&fp.Reason,
-			&fp.UserID,
-			&fp.UserName); err != nil {
-			return nil, err
-		}
-		forecast_points = append(forecast_points, &fp)
-	}
-	return forecast_points, rows.Err()
-}
-
-func (r *PostgresForecastPointRepository) GetOrderedForecastPointsByForecastID(ctx context.Context, id int64) ([]*models.ForecastPoint, error) {
-	query := `SELECT 
-				p.id
-				, p.forecast_id
-				, p.point_forecast
-				, p.reason
-				, p.created
-				, p.user_id
-				, u.username
-				FROM points p 
-				left join users u on p.user_id = u.id
-				WHERE p.forecast_id = $1
-				ORDER BY p.id ASC`
-
-	rows, err := r.db.QueryContext(ctx, query, id)
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	var forecast_points []*models.ForecastPoint
-	for rows.Next() {
-		var fp models.ForecastPoint
-		if err := rows.Scan(&fp.ID,
-			&fp.ForecastID,
-			&fp.PointForecast,
-			&fp.Reason,
-			&fp.CreatedAt,
-			&fp.UserID,
-			&fp.UserName); err != nil {
-			return nil, err
-		}
-		forecast_points = append(forecast_points, &fp)
-	}
-	return forecast_points, rows.Err()
-}
-
-func (r *PostgresForecastPointRepository) GetForecastPointsByDate(ctx context.Context, user_id int64, date *time.Time) ([]*models.ForecastPoint, error) {
-	// Use provided date or default to today
-	targetDate := time.Now()
-	if date != nil {
-		targetDate = *date
-	}
-	// Truncate to start of day for date comparison
-	targetDate = time.Date(targetDate.Year(), targetDate.Month(), targetDate.Day(), 0, 0, 0, 0, targetDate.Location())
-	nextDay := targetDate.AddDate(0, 0, 1)
-
-	query := `SELECT distinct on (forecast_id)
-				p.id
-				, p.forecast_id
-				, p.point_forecast
-				, p.created
-				, p.reason
-				, p.user_id
-				, u.username
-				FROM points p
-				left join users u on p.user_id = u.id
-				WHERE p.user_id = $1
-				AND p.created >= $2
-				AND p.created < $3
-				ORDER BY p.forecast_id, p.created DESC;`
-
-	rows, err := r.db.QueryContext(ctx, query, user_id, targetDate, nextDay)
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	var forecast_points []*models.ForecastPoint
-	for rows.Next() {
-		var fp models.ForecastPoint
-		if err := rows.Scan(&fp.ID,
-			&fp.ForecastID,
-			&fp.PointForecast,
-			&fp.CreatedAt,
-			&fp.Reason,
-			&fp.UserID,
-			&fp.UserName); err != nil {
-			return nil, err
-		}
-		forecast_points = append(forecast_points, &fp)
-	}
-	return forecast_points, rows.Err()
 }
